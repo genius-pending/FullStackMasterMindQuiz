499b582322da18ac7aaa223ffb8f0ecbfa9e49b9
Shivey Singh <shivey_khangura@hotmail.com>
2020-10-11 20:30:34 +0100

    made some changes
    I wasnt happy with the layout of my quiz it seemed all over the place there is a few bugs that need addressing and I wi.l make sure they are sorted out

--- script.js
+++ script.js
@@ -1,57 +1,45 @@
 // connected all the elements from my html file to javascript so that javacript can communicate with my html file
 var startButton = document.getElementById("startquiz")
-var nextButton = document.getElementById("nextbtn")
 var questionContainerEL = document.getElementById("question-container")
 var questionEl = document.getElementById("question")
-var scorebox = document.getElementById("scorebox")
+var scoreBox = document.getElementById("scorebox")
 var A1 = document.getElementById('A')
 var A2 = document.getElementById('B')
 var A3 = document.getElementById('C')
 var A4 = document.getElementById('D')
-var highScore = document.getElementById("highscore")
+var tryAgain = document.getElementById("Try Again")
 var highScoreContainer = document.getElementById("highscore-container")
 var answerfeedback = document.getElementById("answerfeedback")
+
 
 var shuffledQuestions
 var currentQuestionsIndex = 0
 var score = 0
-var selectedButton
+
 
 //used the below function to hide the highscores button until the game is completed
-// once game completed user can click highscore and enter name to view scores.
+// once game completed user can enter name to view scores or replay.
 
-function highFunction() {
-  highScore.classList.add("hide")
-}
 
-highScore.addEventListener("click", function () {
-  highScoreContainer.classList.remove("hide")
-})
-
-function gameOver(){
+function gameOver() {
   clearInterval(timer);
   questionContainerEL.classList.add("hide")
-  startButton.innerText = "Try Again"
-  startButton.classList.remove("hide")
-  highScore.classList.remove("hide")
-  prompt("Please enter you name to view highscore")
-  scorebox.innerText =""
-  score = 0
-  A1.classList.remove("btn2")
-  A2.classList.remove("btn2")
-  A3.classList.remove("btn2")
-  A4.classList.remove("btn2")
+  scoreBox.innerText = "You scored:  " + score + "   out of 10"
   answerfeedback.innerText = ""
-  
+  highScoreContainer.classList.remove("hide")
 }
 
 // added an event listener where by once the start button is clicked it disappears and then the actual quiz and timer pop for the user 
 
-startButton.addEventListener("click", () => {
+startButton.addEventListener("click", startGame);
 
-  startGame()
- 
-})
+
+
+tryAgain.addEventListener("click", startGame);
+
+  console.log(tryAgain)
+
+
 
 //console.log(startButton)
 
@@ -59,14 +47,17 @@ startButton.addEventListener("click", () => {
 
 function startGame() {
   startButton.classList.add("hide")
-  highScore.classList.add("hide")
+  score = 0
   highScoreContainer.classList.add("hide")
+  questionContainerEL.classList.remove("hide")
+
 
   // added a function to generate questions randomly making it harder for the user to memorise 
   //the order of question if they want to retake the quiz
 
   shuffledQuestions = questions.sort(() => Math.random() - .5)
-  questionContainerEL.classList.remove("hide")
+  
+  
   setNextQuestion()
 
   // timer set for 60 seconds once timer elapses an alertbox pops up advising user time is up I then changed the inner html of the start button
@@ -87,8 +78,10 @@ function startGame() {
 
 
 
+
+
   //console.log('started')
- 
+
 
 }
 
@@ -120,42 +113,66 @@ function showQuestion(question) {
 function selectAnswer(e) {
   console.log(e)
   var buttonAnswer = e.target.innerText
-  console.log(selectedButton)
   var currentQuestion = questions[currentQuestionsIndex];
   var answerText = currentQuestion.answers[currentQuestion.correctAnswer];
   if (buttonAnswer == answerText) {
     currentQuestionsIndex++
     setNextQuestion()
-    answerfeedback.style.color="green";
+    answerfeedback.style.color = "green";
     answerfeedback.innerText = "great that was the correct answer"
     score++
-    scorebox.innerText = "Score:  " + score + "   out of 10"
+    
   }
 
   else {
     currentQuestionsIndex++
     setNextQuestion()
-    answerfeedback.style.color="red";
-    // if user gets the wrong answer then 5 seconds is deducted.
     answerfeedback.innerText = "sorry that was wrong 5 seconds have been deducted"
+    answerfeedback.style.color = "red";
+    // if user gets the wrong answer then 5 seconds is deducted.
     countdown -= 5;
+    
+    
 
 
 
   }
+
+  //gets player's name and score and sends to local storage
+
+  var saveScore = document.getElementById("Save Score")
+  saveScore.addEventListener("click", highScores)
+
   
-  //get highscore
+
+  function highScores() {
+  let userName = document.getElementById("Name").value;
+  
+  localStorage.setItem("Name", userName);
+  localStorage.setItem("score", score.JSON.stringify(score));
+  }
+
+  console.log(highScores)
+
   var highscores = JSON.parse(localStorage.getItem("highscores"));
 
-  var scoreboard = {
-
-    score : score
-  
-  }
-
-  
+ 
 
   //console.log(selectAnswer);
+
+  var scoreview= document.getElementById("user-scores")
+  var viewScore= document.getElementById("View Scores")
+  viewScore.addEventListener("click", GetScore)
+
+  function GetScore(){
+    saveScore.classList.add("hide")
+    viewScore.classList.add("hide")
+    userName.classlist.add("hide")
+    h2.innerHTML= "HighScores"
+    p.classlist.add("hide")
+    scoreview.innerText= JSON.parse(localStorage.getItem(userName + score))
+
+  }
 
 }
 
